8874537c5968a9d7300ce2aa6156a64f6b5d426a
gghill <gghill@alumni.stanford.edu>
2026-09-21 11:09:33 +0200

    Updated update message

--- app/appGH.R
+++ app/appGH.R
@@ -4,6 +4,7 @@ library(stringr)
 library(leaflet)
 # data_path <- 'review_results_all_030826.csv'
 data_path <- 'review_results_all_210926.csv'
+# updated 21.09.2026: fixed minor labeling errors in dataset locations
 # updated 10.09.2026: UI wrapper with bslib, toolstip positioning improvement.
 # updated 26.08.2026: consistent direction of effect terminology
 # updated 19.08.2026: fixed subset color labels
